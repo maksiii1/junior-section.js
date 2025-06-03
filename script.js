15e8db94bb27bbc241b6dd8077eff50f7b071bf8
Maksin Spizhuk <indcheat@mail.ru>
2025-06-03 20:30:16 +0300

    update script

--- script.js
+++ script.js
@@ -1,14 +1,72 @@
 const smallData = document.getElementById("btn1");
 const bigData = document.getElementById("btn2");
+const table = document.createElement("table");
 
 smallData.addEventListener("click", () => {
   const containerForTableData = document.getElementsByClassName("dataTable")[0];
 
-  const table = document.createElement("table");
-  table.innerHTML += `<tr><th>Hello!</th></tr>
-  <tr><td>Hello!</td>`;
+  fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      if (table.childNodes) {
+        table.innerHTML = "";
+      }
+
+      let headRow = "<tr>";
+      for (property in json) {
+        headRow += `<th>${property}<span>▲</span></th>`;
+      }
+      headRow += "</tr>";
+      table.innerHTML += headRow;
+
+      let dataRow = "<tr>";
+      for (value in json) {
+        dataRow += `<td>${json[value]}</td>`;
+      }
+      dataRow += "</tr>";
+      table.innerHTML += dataRow;
+    })
+    .catch((error) => console.log(error));
 
   containerForTableData.appendChild(table);
 });
 
-bigData.addEventListener("click", () => {});
+bigData.addEventListener("click", () => {
+  const containerForTableData = document.getElementsByClassName("dataTable")[0];
+
+  fetch(`https://jsonplaceholder.typicode.com/todos`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      if (table.childNodes) {
+        table.innerHTML = "";
+      }
+
+      let headRow = "<tr>";
+      for (obj of json) {
+        for (property in obj) {
+          headRow += `<th>${property}<span>▲</span></th>`;
+        }
+        break;
+      }
+      headRow += "</tr>";
+      table.innerHTML += headRow;
+
+      let dataRow = "";
+      for (obj of json) {
+        dataRow += "<tr>";
+        for (property in obj) {
+          dataRow += `<td>${obj[property]}</td>`;
+        }
+        dataRow += "</tr>";
+      }
+
+      table.innerHTML += dataRow;
+    })
+    .catch((error) => console.log(error));
+
+  containerForTableData.appendChild(table);
+});
