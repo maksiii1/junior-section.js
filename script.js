33b6e9e6e877c13f82ecf26790961970b4cfd988
Maksin Spizhuk <indcheat@mail.ru>
2025-06-06 16:22:25 +0300

    update version

--- script.js
+++ script.js
@@ -73,130 +73,28 @@ bigData.addEventListener("click", () => {
 
 table.addEventListener("click", (event) => {
   const target = event.target;
+
   const tableRowName = target.parentNode;
+
   const tableArrHeaders = target.parentNode.parentNode;
+
   const headers = tableArrHeaders.querySelectorAll("th");
-  const swapNames = Array.from(target.closest("table").querySelectorAll("tr"));
-  const userId = [];
-  const id = [];
-  const titles = [];
-  const completed = [];
+
+  const swapNames = Array.from(
+    target.closest("table").querySelectorAll("tr")
+  ).slice(1);
+
+  const datasJSON = {
+    userId: [],
+    id: [],
+    titles: [],
+    completed: [],
+  };
   let index;
 
   if (target.tagName === "SPAN") {
     if (target.textContent === "▼") {
       target.textContent = "▲";
-
-      for (let i = 0; i < headers.length; i++) {
-        if (headers[i].textContent === tableRowName.textContent) {
-          index = i;
-        }
-      }
-
-      swapNames.forEach((value) => {
-        for (let i = 0; i < value.childNodes.length; i++) {
-          if (value.childNodes[i].tagName !== "TH") {
-            switch (i) {
-              case 0: {
-                userId.push(value.childNodes[i].textContent);
-                break;
-              }
-              case 1: {
-                id.push(value.childNodes[i].textContent);
-                break;
-              }
-              case 2: {
-                titles.push(value.childNodes[i].textContent);
-                break;
-              }
-              case 3: {
-                completed.push(value.childNodes[i].textContent);
-                break;
-              }
-              default: {
-                break;
-              }
-            }
-          }
-        }
-      });
-
-      const userIdSorted = [...userId].sort((a, b) => {
-        return b - a;
-      });
-      const idSorted = [...id].sort((a, b) => {
-        return b - a;
-      });
-      const titlesSorted = [...titles].sort();
-      const completedSorted = [...completed].sort((a, b) => b - a);
-
-      swapNames.forEach((value, ind) => {
-        for (let i = 0; i < value.childNodes.length; i++) {
-          if (value.childNodes[i].tagName !== "TH") {
-            if (i === index) {
-              switch (i) {
-                case 0: {
-                  value.childNodes[i].textContent = userIdSorted[ind];
-
-                  userId.forEach((isValue, index) => {
-                    if (value.childNodes[i].textContent === isValue) {
-                      value.childNodes[i + 1].textContent = id[index];
-                      value.childNodes[i + 2].textContent = titles[index];
-                      value.childNodes[i + 3].textContent = completed[index];
-                    }
-                  });
-                  break;
-                }
-                case 1: {
-                  value.childNodes[i].textContent = idSorted[ind];
-
-                  id.forEach((isValue, index) => {
-                    if (value.childNodes[i].textContent === isValue) {
-                      value.childNodes[i - 1].textContent = userId[index];
-                      value.childNodes[i + 1].textContent = titles[index];
-                      value.childNodes[i + 2].textContent = completed[index];
-                    }
-                  });
-
-                  break;
-                }
-                case 2: {
-                  value.childNodes[i].textContent = titlesSorted[ind];
-
-                  titles.forEach((isValue, index) => {
-                    if (value.childNodes[i].textContent === isValue) {
-                      value.childNodes[i - 2].textContent = userId[index];
-                      value.childNodes[i - 1].textContent = id[index];
-                      value.childNodes[i + 1].textContent = completed[index];
-                    }
-                  });
-
-                  break;
-                }
-                case 3: {
-                  value.childNodes[i].textContent = completedSorted[ind];
-
-                  completed.forEach((isValue, index) => {
-                    if (value.childNodes[i].textContent === isValue) {
-                      value.childNodes[i - 3].textContent = userId[index];
-                      value.childNodes[i - 2].textContent = id[index];
-                      value.childNodes[i - 1].textContent = titles[index];
-                    }
-                  });
-
-                  break;
-                }
-                default: {
-                  break;
-                }
-              }
-            }
-          }
-        }
-      });
-    } else {
-      target.textContent = "▼";
-
       for (let i = 0; i < headers.length; i++) {
         if (headers[i].textContent === tableRowName.textContent) {
           index = i;
@@ -219,6 +117,126 @@ table.addEventListener("click", (event) => {
             if (i === index) {
               // value.childNodes[i].textContent =
               // headerNames[headerNames.length - ind];
+            }
+          }
+        }
+      });
+    } else {
+      target.textContent = "▼";
+
+      for (let i = 0; i < headers.length; i++) {
+        if (headers[i].textContent === tableRowName.textContent) {
+          index = i;
+        }
+      }
+
+      swapNames.forEach((value) => {
+        for (let i = 0; i < value.childNodes.length; i++) {
+          switch (i) {
+            case 0: {
+              datasJSON.userId.push(value.childNodes[i].textContent);
+              break;
+            }
+            case 1: {
+              datasJSON.id.push(value.childNodes[i].textContent);
+              break;
+            }
+            case 2: {
+              datasJSON.titles.push(value.childNodes[i].textContent);
+              break;
+            }
+            case 3: {
+              datasJSON.completed.push(value.childNodes[i].textContent);
+              break;
+            }
+            default: {
+              break;
+            }
+          }
+        }
+      });
+
+      const dataJSONSorted = {
+        userIdSorted: [...datasJSON.userId].sort((a, b) => {
+          return b - a;
+        }),
+        idSorted: [...datasJSON.id].sort((a, b) => {
+          return b - a;
+        }),
+        titlesSorted: [...datasJSON.titles].sort(),
+        completedSorted: [...datasJSON.completed].sort((a, b) => b - a),
+      };
+
+      swapNames.forEach((value, ind) => {
+        for (let i = 0; i < value.childNodes.length; i++) {
+          if (i === index) {
+            switch (i) {
+              case 0: {
+                value.childNodes[i].textContent =
+                  dataJSONSorted.userIdSorted[ind];
+
+                datasJSON.userId.forEach((isValue, index) => {
+                  if (value.childNodes[i].textContent === isValue) {
+                    value.childNodes[i + 1].textContent = datasJSON.id[index];
+                    value.childNodes[i + 2].textContent =
+                      datasJSON.titles[index];
+                    value.childNodes[i + 3].textContent =
+                      datasJSON.completed[index];
+                  }
+                });
+                break;
+              }
+              case 1: {
+                value.childNodes[i].textContent = dataJSONSorted.idSorted[ind];
+
+                datasJSON.id.forEach((isValue, index) => {
+                  if (value.childNodes[i].textContent === isValue) {
+                    value.childNodes[i - 1].textContent =
+                      datasJSON.userId[index];
+                    value.childNodes[i + 1].textContent =
+                      datasJSON.titles[index];
+                    value.childNodes[i + 2].textContent =
+                      datasJSON.completed[index];
+                  }
+                });
+
+                break;
+              }
+              case 2: {
+                value.childNodes[i].textContent =
+                  dataJSONSorted.titlesSorted[ind];
+
+                datasJSON.titles.forEach((isValue, index) => {
+                  if (value.childNodes[i].textContent === isValue) {
+                    value.childNodes[i - 2].textContent =
+                      datasJSON.userId[index];
+                    value.childNodes[i - 1].textContent = datasJSON.id[index];
+                    value.childNodes[i + 1].textContent =
+                      datasJSON.completed[index];
+                  }
+                });
+
+                break;
+              }
+              case 3: {
+                value.childNodes[i].textContent =
+                  dataJSONSorted.completedSorted[ind];
+
+                datasJSON.completed.forEach((isValue, index) => {
+                  if (value.childNodes[i].textContent === isValue) {
+                    value.childNodes[i - 3].textContent =
+                      datasJSON.userId[index];
+                    value.childNodes[i - 2].textContent = datasJSON.id[index];
+                    value.childNodes[i - 1].textContent =
+                      datasJSON.titles[index];
+                  }
+                });
+
+                break;
+              }
+              default: {
+                break;
+              }
             }
           }
         }
